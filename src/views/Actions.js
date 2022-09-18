ebb7ba266501885189eb0fa263ed72085a9184bc
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2022-09-18 11:36:32 +0200

    Ajouter une fonction dans format pour déterminer l'extension d'un nom de fichier (pdf ou image)

--- src/views/Actions.js
+++ src/views/Actions.js
@@ -1,10 +1,10 @@
 import eyeBlueIcon from "../assets/svg/eye_blue.js"
 import downloadBlueIcon from "../assets/svg/download_blue.js"
+import { extFile } from "../app/format.js"
 
 export default (billUrl, fileName) => {
-  // Obtenir l'extension dans le nom du fichier 
-  const arr = /^.+\.([^.]+)$/.exec(fileName);
-  const ext = arr == null ? "" : arr[1].trim().toLowerCase();
+  // pdf ou type image
+  const ext = extFile(fileName);
   return (
     `<div class="icon-actions">
       <div id="eye" data-testid="icon-eye" data-bill-url=${billUrl} data-file-ext=${ext}>
