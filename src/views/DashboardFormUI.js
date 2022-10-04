35bc44025a88dbae22e878a9357d574f6ca019b7
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2022-10-04 17:02:36 +0200

    Ajouter +2 T..U pour tester voir et télécharger un justificatif PDF dans l'administration

--- src/views/DashboardFormUI.js
+++ src/views/DashboardFormUI.js
@@ -10,7 +10,7 @@ export const modal = () => (`
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Justificatif</h5>
+          <h5 class="modal-title" id="exampleModalLongTitle" data-testid="modal-long-title">Justificatif</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
